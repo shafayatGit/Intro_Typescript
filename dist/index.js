3410ac4d4dac0fef60b4a01efc6e24334d0b7fbb
shafayatGit <shafayathossain.ielts@gmail.com>
2025-08-19 11:00:37 +0600

    initialized

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hello world");
+let age = 20;
+// age = "a" //as we declared age as a number we cant change its type anymore
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log("hello world");
+let age: number = 20;
+// age = "a" //as we declared age as a number we cant change its type anymore
+if (age < 50) age += 10; console.log(age)
